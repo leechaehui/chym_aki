619c3aa1800543ca01d6fbe2650c028d1c190dc1
이채희 <cogml0923@gmail.com>
2026-04-30 17:29:51 +0900

    remove unnecessary folders

--- chym_proj(test)/final_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_struct = pd.read_csv("final_features_48h.csv")
-df_nlp = pd.read_csv("nlp_keyword_features.csv")
-
-df_final = df_struct.merge(df_nlp, on="stay_id", how="left")
-
-df_final.to_csv("final_features.csv", index=False)
--- scr/config.py
+++
@@ -1,4 +0,0 @@
-# DB 연결
-import sqlalchemy
-DB_URL = "postgresql://bio4:bio4@localhost:5432/mimic4"
-engine = sqlalchemy.create_engine(DB_URL)
