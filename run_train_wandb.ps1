8e35f6de039457e74fe2e94b716d9f05914413bb
Latte0728 <qkrwjdgkgk12@naver.com>
2026-07-16 08:58:53 +0900

    Weights & Biases 로그 추적용 학습 파워셸 스크립트

--- run_train_wandb.ps1
+++ run_train_wandb.ps1
@@ -7,3 +7,4 @@ $env:PYTHONUTF8 = 1
 
 # 3. Conda chym_proj 환경의 python 실행파일을 직접 구동해 conda run 버그 우회
 & "C:\Users\301-4\anaconda3\envs\chym_proj\python.exe" Pathology_model/mil/train.py --encoder ctranspath --mags 40 --tag exp5
+ 
